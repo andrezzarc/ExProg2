97eea3b64ddbad50ab31e23cdd55367380e1d5db
CamilaBuniac <camilafb4@al.insper.edu.br>
2026-04-13 17:25:22 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,12 @@ def rolar_dados (n):
     for i in range(n):
         lista.append(random.randint(1,6))
     return (lista)
+
+def guardar_dado (rolados,guardados,n):
+    for i in range(len(rolados)):
+        if n == i:
+            guardados.append(rolados[i])
+    res = [rolados, guarados]
+    return res
+
+    
