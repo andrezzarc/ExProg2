eb990a52bc02fffcea7f2cc9f6f41ee4d375c7be
Camila Buniac <camilafb4@al.insper.edu.br>
2026-04-28 18:54:16 -0300

    Funcaoexercicio13

--- funcoes.py
+++ funcoes.py
@@ -150,3 +150,20 @@ def faz_jogada(lista, categoria, dicio):
     else:
         s[int(categoria)] = simples[int(categoria)]
     return dicio
+
+    def imprime_cartela(cartela):
+        print("Cartela de Pontos:")
+        print("-"*25)    
+        for i in range(1, 7):
+            filler = " " * (15 - len(str(i)))
+            if cartela['regra_simples'][i] != -1:
+                print(f"| {i}: {filler}| {cartela['regra_simples'][i]:02} |")
+            else:
+                print(f"| {i}: {filler}|    |")
+        for i in cartela['regra_avancada'].keys():
+            filler = " " * (15 - len(str(i)))
+            if cartela['regra_avancada'][i] != -1:
+                print(f"| {i}: {filler}| {cartela['regra_avancada'][i]:02} |")
+            else:
+                print(f"| {i}: {filler}|    |")
+        print("-"*25)
